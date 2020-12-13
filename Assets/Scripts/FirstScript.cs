165279dd45ff1d83f2b32875ad34a1e9a4859636
aydev <aayush.kumar@famepilot.com>
2020-12-14 00:05:25 +0530

    UI and Stuff

---
+++ Assets/Scripts/FirstScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FirstScript : MonoBehaviour
+{
+    public Button button;
+    public string nextScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button.onClick.AddListener(OnButtonClick);
+        Debug.Log("GameObject Start called");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log("GameObject Update called");
+    }
+
+    void OnButtonClick()
+    {
+        Debug.Log("Button was clicked!");
+        SceneManager.LoadScene(nextScene);
+    }
+}
